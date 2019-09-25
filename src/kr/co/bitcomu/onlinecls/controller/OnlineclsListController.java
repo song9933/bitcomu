d247bc881e81a5f9c05e47e60be31ad9316a3fc4
teamBabyoo <40035841+teamBabyoo@users.noreply.github.com>
2019-09-25 18:10:24 +0900

    온라인강의_메인 페이지 서블릿(컨트롤러)수정

--- src/kr/co/bitcomu/onlinecls/controller/OnlineclsListController.java
+++ src/kr/co/bitcomu/onlinecls/controller/OnlineclsListController.java
@@ -1,7 +1,16 @@
 package kr.co.bitcomu.onlinecls.controller;
 
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
+@WebServlet("/onlineclass/onlinecls_main.do")
 public class OnlineclsListController extends HttpServlet {
-
+	public void service(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		
+	}
 }
