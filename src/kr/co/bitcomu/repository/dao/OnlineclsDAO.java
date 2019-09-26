2160f4c242c90239be52660d053063c6f54c7cdb
유지인 <esther.yoo329@gmail.com>
2019-09-26 13:12:12 +0900

    온라인강의 DAO, xml 수정

--- src/kr/co/bitcomu/repository/dao/OnlineclsDAO.java
+++ src/kr/co/bitcomu/repository/dao/OnlineclsDAO.java
@@ -1,5 +1,11 @@
 package kr.co.bitcomu.repository.dao;
 
-public class OnlineclsDAO {
+import java.util.List;
 
+import kr.co.bitcomu.repository.vo.Onlinecls;
+
+public interface OnlineclsDAO {
+	List<Onlinecls> seletOnlinecls();
+	Onlinecls selectOneOnlinecls(Onlinecls oncls);
+	void insertOnlinecls(Onlinecls oncls);
 }
