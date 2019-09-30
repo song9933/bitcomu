cd8000001849d36e0c02a62dda60aef535cb199c
teamBabyoo <40035841+teamBabyoo@users.noreply.github.com>
2019-09-30 16:33:42 +0900

    Revert "qna,xml qnaDao, qnaController 작성"
    This reverts commit 92cdfd132fbd09e2c1babb19243172687ee53477.

--- src/kr/co/bitcomu/repository/dao/QnaDAO.java
+++ src/kr/co/bitcomu/repository/dao/QnaDAO.java
@@ -2,16 +2,13 @@ package kr.co.bitcomu.repository.dao;
 
 import java.util.List;
 
-import kr.co.bitcomu.repository.vo.Page;
 import kr.co.bitcomu.repository.vo.Qna;
 
 public interface QnaDAO {
 	
-	List<Qna> selectQna(Page page);
-	Qna selectOneQna(int no);
+	List<Qna> selectQnaList();
+	Qna selectOneQna(int qnaNo);
 	void insertQna(Qna qna);
 	void updateQna(Qna qna);
-	void deleteQna(int no);
-	int selectQnaCount();
-
+	void deleteQna(int qnaNo);
 }
