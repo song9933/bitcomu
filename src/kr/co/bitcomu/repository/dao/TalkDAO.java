e06e5ffa283a2dcd6a13aff863c4286c603a8eb5
namsu91 <namsu9191@gmail.com>
2019-09-30 19:46:26 +0900

    sqlMap  mapper 추가

--- src/kr/co/bitcomu/repository/dao/TalkDAO.java
+++ src/kr/co/bitcomu/repository/dao/TalkDAO.java
@@ -1,5 +1,45 @@
+/**
+ * Data 의 처리 클래스
+ * 
+ * CRUD -> 
+ * CREATE  : insert + 기능
+ * READ    : select + 기능,  selectOne + 기능
+ * UPDATE  : update + 기능
+ * DELETE  : delete + 기능
+ */
 package kr.co.bitcomu.repository.dao;
 
-public interface TalkDAO {
+import java.util.List;
 
+import kr.co.bitcomu.repository.vo.Comment;
+import kr.co.bitcomu.repository.vo.Talk;
+
+
+public interface TalkDAO {
+	List<Talk> selectTalk();
+	Talk selectOneTalk(int no);
+	void updateViewCnt(int no);
+	void insertTalk(Talk talk);
+	void updateTalk(Talk talk);
+	int deleteTalk(int no);
+	
+	/* 댓글 파트 시작 ========================================= */
+	List<Comment> selectComment(int no);
+	void insertComment(Comment comment);
+	void deleteComment(int commentNo);
+	void updateComment(Comment comment);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
