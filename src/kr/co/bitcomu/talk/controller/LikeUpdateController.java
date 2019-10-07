9c7f1bd12d869987f0d543b3e24595817fe45874
namsu91 <namsu9191@gmail.com>
2019-10-07 20:50:21 +0900

    추천기능 누락된 파일

---
+++ src/kr/co/bitcomu/talk/controller/LikeUpdateController.java
@@ -0,0 +1,55 @@
+package kr.co.bitcomu.talk.controller;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import kr.co.bitcomu.common.db.MyAppSqlConfig;
+import kr.co.bitcomu.repository.dao.TalkDAO;
+import kr.co.bitcomu.repository.vo.Like;
+import kr.co.bitcomu.repository.vo.Talk;
+
+@WebServlet("/talk/like_update.do")
+public class LikeUpdateController extends HttpServlet{
+	private TalkDAO dao; 
+		
+	public LikeUpdateController() {
+		dao = MyAppSqlConfig.getSqlSessionInstance().getMapper(TalkDAO.class);
+	}
+
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) 
+			throws ServletException, IOException {
+		
+		Talk talk = new Talk();
+		Map<String, Object> map = new HashMap<>();
+		map.put("boardPostNo", Integer.parseInt(req.getParameter("postNo")));
+		map.put("userNo", Integer.parseInt(req.getParameter("userNo")));
+		
+		dao.insertTalkLikeCount(map);
+		
+		
+//		Like like = new Like();
+//		like.setBoardPostNo(Integer.parseInt(req.getParameter("postNo")));
+//		like.setUserNo(Integer.parseInt(req.getParameter("userNo")));
+		 
+		talk.setLikeCnt(dao.selectTalkLikeCount(Integer.parseInt(req.getParameter("postNo"))));
+		
+		dao.updateLikeCnt(Integer.parseInt(req.getParameter("postNo")));
+		
+		resp.sendRedirect("/bitcomu/talk_detail.do?postNo=" + Integer.parseInt(req.getParameter("postNo")) 
+		  									   + "&pageNo=" + Integer.parseInt(req.getParameter("pageNo")) 
+		
+);
+	}
+	
+	
+	
+	
+}
