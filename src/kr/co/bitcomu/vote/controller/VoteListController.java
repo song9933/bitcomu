90855d0329076fbf265b30ecbe23c2bd43269e90
dongchanhan <han3058@gmail.com>
2019-10-10 20:44:05 +0900

    투표리스트 출력 무한스크롤 구현중
    ..

--- src/kr/co/bitcomu/vote/controller/VoteListController.java
+++ src/kr/co/bitcomu/vote/controller/VoteListController.java
@@ -1,6 +1,7 @@
 package kr.co.bitcomu.vote.controller;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.List;
 
 import javax.servlet.ServletException;
@@ -8,6 +9,8 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
 
 import kr.co.bitcomu.common.db.MyAppSqlConfig;
 import kr.co.bitcomu.repository.dao.VoteDAO;
@@ -22,10 +25,14 @@ public class VoteListController extends HttpServlet{
 	}
 	@Override
 	protected void service(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		
 		List<Vote> list = dao.selectVote();
 		for(Vote v : list) {
 			v.setUserId(dao.selectUserId(v.getVoteNo()));
 		}
+//		PrintWriter out = res.getWriter();
+//		out.println(new Gson().toJson(list));
+//		out.close();
 		req.setAttribute("list", list);
 		req.getRequestDispatcher("/jsp/vote/votelist.jsp").forward(req, res);
 	}
