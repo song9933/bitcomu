5608478363220d93536655ff758dcca1d2ca58f3
devhan90 <han3058@gmail.com>
2019-10-12 23:38:17 +0900

    XSS 공격 방어 필터 구현
    ..

---
+++ src/kr/co/bitcomu/filter/CrossScriptingFilter.java
@@ -0,0 +1,36 @@
+package kr.co.bitcomu.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+
+public class CrossScriptingFilter implements Filter{
+	
+	private FilterConfig filterConfig = null;
+
+
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+		this.filterConfig = filterConfig;
+	}
+
+	@Override
+	public void destroy() {
+		this.filterConfig = null;
+	}
+
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		
+		chain.doFilter(new RequestWrapper((HttpServletRequest)request), response);
+	}
+
+}
